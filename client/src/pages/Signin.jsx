460dc0b36e41f23eac578972d61d7628897dd9f0
Naman Pandey <2492004namanpandey@gmail.com>
2024-01-26 10:42:34 +0530

    Added redux toolkit

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,13 +1,14 @@
 import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react'
 import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
+import { signInStart, signInSuccess, signInFailure } from '../redux/user/userSlice'
+import { useDispatch , useSelector} from 'react-redux'
 
 const Signin = () => {
 
     const [formData, setFormData] =  useState({});
-    const [errorMessage, setErrorMessage] = useState(null);
-    const [loading, setLoading] = useState(false);
-
+    const dispatch = useDispatch();
+    const {loading, error: errorMessage} = useSelector(state => state.user);
     const navigate = useNavigate();
 
     const handleChange = (e) => {
@@ -17,11 +18,10 @@ const Signin = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         if( !formData.email || !formData.password){
-            return setErrorMessage('Please fill out all fields.');
+            return dispatch(signInFailure('Please fill out all fields.'));
         }
         try {
-            setLoading(true);
-            setErrorMessage(null);
+            dispatch(signInStart());
             const res = await fetch('/api/auth/signin',{
                 method: 'POST',
                 headers: {'Content-Type': 'application/json'},
@@ -29,15 +29,14 @@ const Signin = () => {
             });
             const data = await res.json();
             if (data.success === false) {
-                return setErrorMessage(data.message);
+                dispatch(signInFailure(data.message));
             }
-            setLoading(false);
             if(res.ok){
+                dispatch(signInSuccess(data));
                 navigate('/');
             }
         } catch (error) {
-            setErrorMessage(error.message);
-            setLoading(false);
+            dispatch(signInFailure(error.message));
         }
     }
 
@@ -76,7 +75,7 @@ const Signin = () => {
                                         <Spinner size='sm'/>
                                         <span className='pl-3'>Loading...</span>
                                     </>
-                                )  : 'Sign Up'
+                                )  : 'Sign In'
                             }
                         </Button>
                     </form>
