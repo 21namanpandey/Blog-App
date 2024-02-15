b359d51dff27969efdb3afab034eaa23a9a0ada2
Naman Pandey <2492004namanpandey@gmail.com>
2024-02-15 23:19:10 +0530

    Add recent article section to the post page

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -4,14 +4,16 @@ import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom"
 import CallToAction from "../components/CallToAction";
 import CommentSection from "../components/CommentSection";
+import PostCard from "../components/PostCard";
 
 
 const PostPage = () => {
 
-    const {postSlug} = useParams();
+    const { postSlug } = useParams();
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(false);
     const [post, setPost] = useState(null);
+    const [recentPosts, setRecentPosts] = useState(null);
 
     useEffect(() => {
         const fetchPost = async () => {
@@ -35,7 +37,22 @@ const PostPage = () => {
             }
         }
         fetchPost();
-    },[postSlug]);
+    }, [postSlug]);
+
+    useEffect(() => {
+        try {
+            const fetchRecentPosts = async () => {
+                const res = await fetch(`/api/post/getposts?limit=3`);
+                const data = await res.json();
+                if (res.ok) {
+                    setRecentPosts(data.posts);
+                }
+            };
+            fetchRecentPosts();
+        } catch (error) {
+            console.log(error.message);
+        }
+    }, []);
 
     if (loading) {
         return (
@@ -51,18 +68,26 @@ const PostPage = () => {
             <Link to={`/search?category=${post && post.category}`} className="self-center mt-5">
                 <Button color="gray" pill size='xs'>{post && post.category}</Button>
             </Link>
-            <img src={post && post.image} alt={post && post.title} className="mt-10 p-3 max-h-[600px] w-full object-cover"/>
+            <img src={post && post.image} alt={post && post.title} className="mt-10 p-3 max-h-[600px] w-full object-cover" />
             <div className="flex justify-between p-3 border-b border-slate-500 mx-auto w-full max-w-2xl text-xs ">
                 <span>{post && new Date(post.createdAt).toLocaleDateString()}</span>
-                <span className="italic">{post && (post.content.length / 1000).toFixed(0) } mins read</span>
+                <span className="italic">{post && (post.content.length / 1000).toFixed(0)} mins read</span>
             </div>
-            <div className="p-3 max-w-2xl mx-auto w-full post-content" dangerouslySetInnerHTML={{__html: post && post.content}}>
-                
+            <div className="p-3 max-w-2xl mx-auto w-full post-content" dangerouslySetInnerHTML={{ __html: post && post.content }}>
+
             </div>
             <div className="max-w-4xl mx-auto w-full">
-                <CallToAction  />
+                <CallToAction />
             </div>
             <CommentSection postId={post._id} />
+
+            <div className='flex flex-col justify-center items-center mb-5'>
+                <h1 className='text-xl mt-5'>Recent articles</h1>
+                <div className='flex flex-wrap gap-5 mt-5 justify-center'>
+                    {recentPosts &&
+                        recentPosts.map((post) => <PostCard key={post._id} post={post} />)}
+                </div>
+            </div>
         </main>
     )
 }
