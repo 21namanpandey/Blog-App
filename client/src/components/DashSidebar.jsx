93568abbe1c60105dc027f088315d1b4478b7352
Naman Pandey <2492004namanpandey@gmail.com>
2024-02-16 20:27:11 +0530

    Add dashboard overview to the admin dashboard

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,6 +1,6 @@
 import { Sidebar } from "flowbite-react"
 import { useEffect, useState } from "react";
-import { HiAnnotation, HiArrowSmRight, HiDocumentText, HiOutlineUserGroup, HiUser } from 'react-icons/hi'
+import { HiAnnotation, HiArrowSmRight, HiChartPie, HiDocumentText, HiOutlineUserGroup, HiUser } from 'react-icons/hi'
 import { Link, useLocation } from "react-router-dom";
 import { signoutSuccess } from "../redux/user/userSlice";
 import { useDispatch } from "react-redux";
@@ -11,7 +11,7 @@ const DashSidebar = () => {
 
     const location = useLocation();
     const dispatch = useDispatch();
-    const {currentUser} = useSelector((state) => state.user);
+    const { currentUser } = useSelector((state) => state.user);
     const [tab, setTab] = useState('');
     useEffect(() => {
         const urlParams = new URLSearchParams(location.search)
@@ -42,6 +42,19 @@ const DashSidebar = () => {
         <Sidebar className="w-full md:w-56 ">
             <Sidebar.Items>
                 <Sidebar.ItemGroup className='flex flex-col gap-1'>
+                    {
+                        currentUser && currentUser.isAdmin && (
+                            <Link to={`/dashboard?tab=dash`}>
+                                <Sidebar.Item
+                                    active={tab === 'dash' || !tab}
+                                    icon={HiChartPie}
+                                    as='div'
+                                >
+                                    DashBoard
+                                </Sidebar.Item>
+                            </Link>
+                        )
+                    }
                     <Link to='/dashboard?tab=profile'>
                         <Sidebar.Item active={tab === 'profile'} icon={HiUser} label={currentUser.isAdmin ? 'Admin' : 'User'} labelColor='dark' as='div' >
                             Profile
@@ -61,23 +74,23 @@ const DashSidebar = () => {
                     {currentUser.isAdmin && (
                         <>
                             <Link to='/dashboard?tab=users'>
-                            <Sidebar.Item
-                                active={tab === 'users'}
-                                icon={HiOutlineUserGroup}
-                                as='div'
-                            >
-                                Users
-                            </Sidebar.Item>
-                        </Link>
-                        <Link to='/dashboard?tab=comments'>
-                            <Sidebar.Item
-                                active={tab === 'comments'}
-                                icon={HiAnnotation}
-                                as='div'
-                            >
-                                Comments
-                            </Sidebar.Item>
-                        </Link>
+                                <Sidebar.Item
+                                    active={tab === 'users'}
+                                    icon={HiOutlineUserGroup}
+                                    as='div'
+                                >
+                                    Users
+                                </Sidebar.Item>
+                            </Link>
+                            <Link to='/dashboard?tab=comments'>
+                                <Sidebar.Item
+                                    active={tab === 'comments'}
+                                    icon={HiAnnotation}
+                                    as='div'
+                                >
+                                    Comments
+                                </Sidebar.Item>
+                            </Link>
                         </>
                     )}
                     <Sidebar.Item icon={HiArrowSmRight} className='cursor-pointer' onClick={handleSignout} >
